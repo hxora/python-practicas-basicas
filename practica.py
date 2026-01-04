0fdacad4d2e3021ca95457df2d4c7663561fe22e
hxora <hxoradev@gmail.com>
2026-01-03 23:18:42 -0500

    Add files via upload

---
+++ practica.py
@@ -0,0 +1,33 @@
+print ("Esto es una practica")
+print (" ")
+nombre = "Violeta"
+print ("Mi nombre es: " + nombre)
+
+print (" ")
+nombre = "Carlos"
+edad = 16
+print ("Mi nombre es " + nombre + " y tengo " + str(edad) + " años")
+
+colegio = "Santiago Antúnez de Mayolo"
+print(len(colegio))
+
+texto = "Hola world"
+print(texto.upper())
+
+texto = "Hola Mundo"
+print(texto.lower())
+
+palabra = "PYthon"
+print(palabra[0])
+
+palabra = "manzana"
+print(palabra.replace("a", "o"))
+
+nombre = input("ingresa tu nombre: ")
+print ("Hola", nombre)
+
+palabra = input("ingresa una palabra y veras cuantas letras tiene: ")
+print(len(palabra))
+
+palabra = input("escribe una palabra y veras algo: ")
+print (palabra[::-1])
