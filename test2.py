99d4655b4457164e4db19743a1e8887426f93080
hxora <hxoradev@gmail.com>
2026-01-03 23:21:17 -0500

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,52 @@
+print("PREGUNTA 1")
+saludo = "Hola python"
+print(saludo)
+print (" ")
+
+print("PREGUNTA 2")
+nombre = input("proporcioname tu nombre plis: ")
+print ("Hola estimado", nombre)
+print (" ")
+
+print("PREGUNTA 3")
+palabra = input("Dame una palabra y te dire cuantas letras tiene: ")
+print(len(palabra))
+print(" ")
+
+print("PREGUNTA 4")
+palabra = input("Dame una palabra y la pondre en mayus: ")
+print(palabra.upper())
+print (" ")
+
+print("PREGUNTA 5")
+palabra = input("ahora una palabra y la pondre en minusculas: ")
+print(palabra.lower())
+print (" ")
+
+print("PREGUNTA 6")
+print("Ahora la primera y ultima letra de python: ")
+palabra = "Python"
+print(palabra[0])
+print(palabra[5])
+print (" ")
+
+print("PREGUNTA 7")
+letra = input("dame una palabra con muchas e y los cambiare: ")
+print(letra.replace("e", "3"))
+print (" ")
+
+print("PREGUNTA 8")
+nombre = input("tu nombre pli: ")
+edad = input("tu edad plox: ")
+print("Tu nombre es", nombre, "y tienes", edad, "años")
+print(" ")
+
+print("PREGUNTA 9")
+palabra = input("Dame una palabra y veras algo: ")
+print(palabra[::-1])
+print(" ")
+
+print("PREGUNTA 10")
+frase = input("Dame una frase y ya bro: ")
+print(frase)
+print(len(frase))
